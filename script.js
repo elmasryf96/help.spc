0b64626cb2f0845f8d14b500df0c2c7c4a92355e
elmasryf96 <elmasryf96@gmail.com>
2026-08-14 16:01:51 +0400

    script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function handleLogin(event) {
 
     if (user === "SPC" && pass === "SPC@2026") {
         errorMsg.style.display = "none";
-        navigateTo('home-page');
+        navigateTo('towers-page');
     } else {
         errorMsg.style.display = "block";
     }
